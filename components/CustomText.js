33429484aceeb0fe9b19d199af67ebec8b6b8835
Shajwan <shajwannamiqsalh@gmail.com>
2022-12-13 23:34:41 +0300

    adding About section

--- components/CustomText.js
+++ components/CustomText.js
@@ -1,9 +1,18 @@
-import React from 'react'
+"use client";
 
-export default function CustomText() {
-  return (
-    <div>CustomText</div>
-  )
-}
+import { motion } from "framer-motion";
+import { textContainer, textVariant2 } from "../utils/motion";
+import React from "react";
 
-
+export const TypingText = ({ title }) => (
+  <motion.p
+    variants={textContainer}
+    className={`font-normal text-[14px] text-secondary-white `}
+  >
+    {Array.from(title).map((letter, index) => (
+      <motion.span variants={textVariant2} key={index}>
+        {letter === " " ? "\u00A0" : letter}
+      </motion.span>
+    ))}
+  </motion.p>
+);
--- utils/motion.js
+++ utils/motion.js
@@ -88,3 +88,47 @@ export const footerVariants = {
   },
 };
  
+
+export const textContainer = {
+  hidden: {
+    opacity: 0,
+  },
+  show: (i = 1) => ({
+    opacity: 1,
+    transition: { staggerChildren: 0.1, delayChildren: i * 0.1 },
+  }),
+};
+
+export const textVariant2 = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "tween",
+      ease: "easeIn",
+    },
+  },
+};
+
+ export const fadeIn = (direction, type, delay, duration) => ({
+   hidden: {
+     x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
+     y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+     opacity: 0,
+   },
+   show: {
+     x: 0,
+     y: 0,
+     opacity: 1,
+     transition: {
+       type,
+       delay,
+       duration,
+       ease: "easeOut",
+     },
+   },
+ });
