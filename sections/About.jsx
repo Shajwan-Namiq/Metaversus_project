6cab3e09350f531baae8d30e545a0e253d4538ab
Shajwan <shajwannamiqsalh@gmail.com>
2022-12-13 23:57:00 +0300

    update

--- sections/About.jsx
+++ sections/About.jsx
@@ -11,49 +11,53 @@ import React from "react";
 const About = () => {
   return (
     <>
-      <motion.div
-        variants={staggerContainer}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: false, amount: 0.25 }}
-        className="flex-col"
-      >
-        <div
-          className={`${styles.padding}   flex-col justify-center items-center flex-shrink`}
+      <section className={` relative z-10`}>
+        <div className="gradient-02 z-0" />
+        <div className="absolute w-[50%] h-[50%] inset-9 opacity-30 top-10 left-0 gradient-01 " />
+
+        <motion.div
+          variants={staggerContainer}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: false, amount: 0.25 }}
+          className="flex-col"
         >
-          <h2 className="text-center mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
-            <TypingText title="| About Metaversus" />
-          </h2>
-
-          <motion.p
-            variants={fadeIn("up", "tween", 0.2, 1)}
-            className="mb-6 text-base  text-center text-indigo-100 md:text-[32px] leading-relaxed"
+          <div
+            className={`${styles.padding}   flex-col justify-center items-center flex-shrink`}
           >
-            <span className="font-extrabold">Metaverse</span> is a new thing in
-            the future, where you can enjoy the virtual world by feeling like
-            it's really real, you can feel what you feel in this metaverse
-            world, because this is really the{" "}
-            <span className="font-extrabold">madness of the metaverse </span>of
-            today, using only <span className="font-extrabold">VR</span> devices
-            you can easily explore the metaverse world you want, turn your
-            dreams into reality.Let's{" "}
-            <span className="font-extrabold">explore</span> the madness of the
-            metaverse by scrolling down{" "}
-          </motion.p>
+            <h2 className="text-center mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
+              <TypingText title="| About Metaversus" />
+            </h2>
 
-        <div className="flex justify-center">
-            <motion.img
-            variants={fadeIn("up", "tween", 0.3, 1)}
-            src="/arrow-down.svg"
-            alt="arrow down"
-            className="w-[18px] h-[28px]  object-contain mt-[28px]"
-          />
-        </div>
-        
+            <motion.p
+              variants={fadeIn("up", "tween", 0.2, 1)}
+              className="mb-6 text-base  text-center text-indigo-100 md:text-[32px] leading-relaxed"
+            >
+              <span className="font-extrabold">Metaverse</span> is a new thing
+              in the future, where you can enjoy the virtual world by feeling
+              like it's really real, you can feel what you feel in this
+              metaverse world, because this is really the{" "}
+              <span className="font-extrabold">madness of the metaverse </span>
+              of today, using only <span className="font-extrabold">
+                VR
+              </span>{" "}
+              devices you can easily explore the metaverse world you want, turn
+              your dreams into reality.Let's{" "}
+              <span className="font-extrabold">explore</span> the madness of the
+              metaverse by scrolling down{" "}
+            </motion.p>
 
-
-        </div>
-      </motion.div>
+            <div className="flex justify-center">
+              <motion.img
+                variants={fadeIn("up", "tween", 0.3, 1)}
+                src="/arrow-down.svg"
+                alt="arrow down"
+                className="w-[18px] h-[28px]  object-contain mt-[28px]"
+              />
+            </div>
+          </div>
+        </motion.div>
+      </section>
     </>
   );
 };
