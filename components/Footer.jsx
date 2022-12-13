55b2cfae9ccbecae80dac7d383ab2401545f1627
Shajwan <shajwannamiqsalh@gmail.com>
2022-12-13 13:29:33 +0300

    update navbar

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,24 +2,85 @@ import React from "react";
 import { useState } from "react";
 
 const Footer = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  return (
-    <>
-     
-
-
-
-
-
-
-
-
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque aspernatur aut error nobis nihil unde obcaecati! Ipsam sit magnam maxime cum blanditiis ipsum illum? Aliquam minus quis ipsa non assumenda.
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur cum quibusdam ab nesciunt eaque qui aut, doloremque sequi error inventore corrupti ullam voluptate molestias, mollitia cumque autem illo quaerat praesentium.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias tempore placeat fuga unde dolor ipsa, officiis corrupti illo ab cumque fugit, asperiores necessitatibus aperiam in, aliquid quis! Minus, reiciendis autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo, corporis perspiciatis numquam officia veniam commodi a atque, adipisci facere expedita aliquam quos quas nostrum rem, sapiente ullam. Perferendis, minima cumque.
-    </>
-  );
+   return (
+     <>
+       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque
+       aspernatur aut error nobis nihil unde obcaecati! Ipsam sit magnam maxime
+       cum blanditiis ipsum illum? Aliquam minus quis ipsa non assumenda. Lorem
+       ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur cum
+       quibusdam ab nesciunt eaque qui aut, doloremque sequi error inventore
+       corrupti ullam voluptate molestias, mollitia cumque autem illo quaerat
+       praesentium. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+       Alias tempore placeat fuga unde dolor ipsa, officiis corrupti illo ab
+       cumque fugit, asperiores necessitatibus aperiam in, aliquid quis! Minus,
+       reiciendis autem! Lorem ipsum dolor, sit amet consectetur adipisicing
+       elit. Quo, corporis perspiciatis numquam officia veniam commodi a atque,
+       adipisci facere expedita aliquam quos quas nostrum rem, sapiente ullam.
+       Perferendis, minima cumque. Lorem ipsum dolor sit amet consectetur,
+       adipisicing elit. Eaque aspernatur aut error nobis nihil unde obcaecati!
+       Ipsam sit magnam maxime cum blanditiis ipsum illum? Aliquam minus quis
+       ipsa non assumenda. Lorem ipsum dolor sit amet consectetur, adipisicing
+       elit. Consequuntur cum quibusdam ab nesciunt eaque qui aut, doloremque
+       sequi error inventore corrupti ullam voluptate molestias, mollitia cumque
+       autem illo quaerat praesentium. Lorem ipsum dolor sit, amet consectetur
+       adipisicing elit. Alias tempore placeat fuga unde dolor ipsa, officiis
+       corrupti illo ab cumque fugit, asperiores necessitatibus aperiam in,
+       aliquid quis! Minus, reiciendis autem! Lorem ipsum dolor, sit amet
+       consectetur adipisicing elit. Quo, corporis perspiciatis numquam officia
+       veniam commodi a atque, adipisci facere expedita aliquam quos quas
+       nostrum rem, sapiente ullam. Perferendis, minima cumque. Lorem ipsum
+       dolor sit amet consectetur, adipisicing elit. Eaque aspernatur aut error
+       nobis nihil unde obcaecati! Ipsam sit magnam maxime cum blanditiis ipsum
+       illum? Aliquam minus quis ipsa non assumenda. Lorem ipsum dolor sit amet
+       consectetur, adipisicing elit. Consequuntur cum quibusdam ab nesciunt
+       eaque qui aut, doloremque sequi error inventore corrupti ullam voluptate
+       molestias, mollitia cumque autem illo quaerat praesentium. Lorem ipsum
+       dolor sit, amet consectetur adipisicing elit. Alias tempore placeat fuga
+       unde dolor ipsa, officiis corrupti illo ab cumque fugit, asperiores
+       necessitatibus aperiam in, aliquid quis! Minus, reiciendis autem! Lorem
+       ipsum dolor, sit amet consectetur adipisicing elit. Quo, corporis
+       perspiciatis numquam officia veniam commodi a atque, adipisci facere
+       expedita aliquam quos quas nostrum rem, sapiente ullam. Perferendis,
+       minima cumque. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+       Eaque aspernatur aut error nobis nihil unde obcaecati! Ipsam sit magnam
+       maxime cum blanditiis ipsum illum? Aliquam minus quis ipsa non assumenda.
+       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur
+       cum quibusdam ab nesciunt eaque qui aut, doloremque sequi error inventore
+       corrupti ullam voluptate molestias, mollitia cumque autem illo quaerat
+       praesentium. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+       Alias tempore placeat fuga unde dolor ipsa, officiis corrupti illo ab
+       cumque fugit, asperiores necessitatibus aperiam in, aliquid quis! Minus,
+       reiciendis autem! Lorem ipsum dolor, sit amet consectetur adipisicing
+       elit. Quo, corporis perspiciatis numquam officia veniam commodi a atque,
+       adipisci facere expedita aliquam quos quas nostrum rem, sapiente ullam.
+       Perferendis, minima cumque. Lorem ipsum dolor sit amet consectetur,
+       adipisicing elit. Eaque aspernatur aut error nobis nihil unde obcaecati!
+       Ipsam sit magnam maxime cum blanditiis ipsum illum? Aliquam minus quis
+       ipsa non assumenda. Lorem ipsum dolor sit amet consectetur, adipisicing
+       elit. Consequuntur cum quibusdam ab nesciunt eaque qui aut, doloremque
+       sequi error inventore corrupti ullam voluptate molestias, mollitia cumque
+       autem illo quaerat praesentium. Lorem ipsum dolor sit, amet consectetur
+       adipisicing elit. Alias tempore placeat fuga unde dolor ipsa, officiis
+       corrupti illo ab cumque fugit, asperiores necessitatibus aperiam in,
+       aliquid quis! Minus, reiciendis autem! Lorem ipsum dolor, sit amet
+       consectetur adipisicing elit. Quo, corporis perspiciatis numquam officia
+       veniam commodi a atque, adipisci facere expedita aliquam quos quas
+       nostrum rem, sapiente ullam. Perferendis, minima cumque. Lorem ipsum
+       dolor sit amet consectetur, adipisicing elit. Eaque aspernatur aut error
+       nobis nihil unde obcaecati! Ipsam sit magnam maxime cum blanditiis ipsum
+       illum? Aliquam minus quis ipsa non assumenda. Lorem ipsum dolor sit amet
+       consectetur, adipisicing elit. Consequuntur cum quibusdam ab nesciunt
+       eaque qui aut, doloremque sequi error inventore corrupti ullam voluptate
+       molestias, mollitia cumque autem illo quaerat praesentium. Lorem ipsum
+       dolor sit, amet consectetur adipisicing elit. Alias tempore placeat fuga
+       unde dolor ipsa, officiis corrupti illo ab cumque fugit, asperiores
+       necessitatibus aperiam in, aliquid quis! Minus, reiciendis autem! Lorem
+       ipsum dolor, sit amet consectetur adipisicing elit. Quo, corporis
+       perspiciatis numquam officia veniam commodi a atque, adipisci facere
+       expedita aliquam quos quas nostrum rem, sapiente ullam. Perferendis,
+       minima cumque.
+     </>
+   );
 };
 
 export default Footer;
