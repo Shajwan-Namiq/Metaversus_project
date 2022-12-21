be4f33c3a86ef5db89adc44b9fce5ccb982f07a9
Shajwan <shajwannamiqsalh@gmail.com>
2022-12-21 13:35:20 +0300

    update

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import "../styles/navbar.css";
+
 import { navVariants } from "../utils/motion";
 import { useState } from "react";
 import { SidebarData } from "../constants";
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 
 import "../styles/globals.css";
+import "../styles/navbar.css";
 
 const MyApp = ({ Component, pageProps }) => (
   <>
